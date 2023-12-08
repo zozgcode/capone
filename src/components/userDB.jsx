5ac88038b5e219ff58f3d0ddc9c2500ce0de22b2
sunshine-web-coder <webdev4gaming@gmail.com>
2023-12-08 06:51:17 +0100

    nn

--- src/components/userDB.jsx
+++ src/components/userDB.jsx
@@ -3,9 +3,11 @@ const users = [
     id: 1,
     name: "Hendrix Edward Billy",
     cardName: "Hendrix Edward B.",
-    username: "Hendrixedwardbilly",
-    password: "Hendrixbill10",
+    username: "dfiefha4mf83f",
+    password: "dsdfamfeuafa4",
     amount: "99,000,000.00",
   },
 ];
 export default users;
+// username: "Hendrixedwardbilly",
+// password: "Hendrixbill10",
